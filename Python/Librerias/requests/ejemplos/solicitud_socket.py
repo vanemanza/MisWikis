e96f586a4f8144d098c08a52272ec3b3c81f4704
vanesa.manzanelli <vanesa.manzanelli@fluxit.com.ar>
2023-07-02 13:10:18 -0300

    ejemplo solicitud con socket de python

---
+++ Python/Librerias/requests/ejemplos/solicitud_socket.py
@@ -0,0 +1,30 @@
+import socket
+
+# Definir los detalles de la solicitud
+host = 'api.example.com'
+port = 80
+path = '/'
+
+# Crear un socket TCP
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Conectar al servidor
+sock.connect((host, port))
+
+# Enviar la solicitud HTTP
+request = f"GET {path} HTTP/1.1\r\nHost: {host}\r\n\r\n"
+sock.sendall(request.encode())
+
+# Recibir la respuesta
+response = b''
+while True:
+    data = sock.recv(4096)
+    if not data:
+        break
+    response += data
+
+# Cerrar la conexión
+sock.close()
+
+# Imprimir la respuesta
+print(response.decode())
